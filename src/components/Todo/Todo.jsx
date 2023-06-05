5b6fc868a9746a432aa75a24664fa743a42e3691
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-06-05 23:29:15 +0530

    deleteCurrentItem() fixed using  map for displaying components

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,25 +3,23 @@ import './Todo.css'
 import { TodoItem } from './TodoItem/TodoItem'
 
 export const Todo = () => {
-    const [itemCount, setItemCount] = useState([])
     const [todoContent, setTodoContent] = useState([])
 
-    const addNewItem = () => {
-        if (itemCount.length < 5) {
-            setItemCount(prev => [...prev, true]);
-        }
-    }
+
+
     const inputHandleOnBlur = (event) => {
-
         setTodoContent(prev => [...prev, event.target.value]);
-
-
     }
 
-    const deleteCurrentItem=()=>{
-        setItemCount(prev=>[prev.splice(1,0,false)]);
+    const deleteCurrentItem = (key) => {
+        todoContent.splice(key, 1);
+        const filteredContent = todoContent.filter((data) => {
+            return data;
+        })
+        setTodoContent(filteredContent);
     }
-    console.log(todoContent);
+
+    // console.log(todoContent);
 
     return (
         <div>
@@ -30,22 +28,17 @@ export const Todo = () => {
 
                 <div className="input-container">
                     <input type="text" name="newTodo" id="newTodo" placeholder='New Todo' onBlur={inputHandleOnBlur} />
-                    <button type='submit' onClick={addNewItem}>ADD TODO</button>
+                    <button type='submit' >ADD TODO</button>
                 </div>
                 <div className="todo-list" >
-                    {/* {itemCount[0] && <TodoItem todoContent={todoContent[0]} deleteCurrentItem={deleteCurrentItem}/>}
-                    {itemCount[1] && <TodoItem todoContent={todoContent[1]} deleteCurrentItem={deleteCurrentItem}/>}
-                    {itemCount[2] && <TodoItem todoContent={todoContent[2]} deleteCurrentItem={deleteCurrentItem}/>}
-                    {itemCount[3] && <TodoItem todoContent={todoContent[3]} deleteCurrentItem={deleteCurrentItem}/>}
-                    {itemCount[4] && <TodoItem todoContent={todoContent[4]} deleteCurrentItem={deleteCurrentItem}/>}
-                    {itemCount[5] && <TodoItem todoContent={todoContent[5]} deleteCurrentItem={deleteCurrentItem}/>}
-                    {itemCount[6] && <TodoItem todoContent={todoContent[6]} deleteCurrentItem={deleteCurrentItem}/>} */}
 
-                    {todoContent.map((data)=>{
-                        return <TodoItem todoContent={data} deleteCurrentItem={deleteCurrentItem} key={data} />
+                    {todoContent.map((data) => {
+                        return (<TodoItem todoContent={data}
+                            deleteCurrentItem={() => { deleteCurrentItem(todoContent.indexOf(data)) }}
+                            key={todoContent.indexOf(data)} />)
                     })}
                 </div>
-                
+
 
             </div>
 
